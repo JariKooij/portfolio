cf8fa131f28436d41644c06f570710923d7a25cf
JariKooij <jarikooij@gmail.com>
2023-01-02 23:04:41 +0100

    footernav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useWindowBlurChangeTitle } from "./utils/useWindowBlurChangeTitle";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   useWindowBlurChangeTitle('Please, come back!');
 
   return (
     <div className="App">
-      <div id="wrapper">
-        <main>
+      <BrowserRouter>
+        <div id="wrapper">
+          <main>
+              <Routes>
+                <Route path="/" element={ <p>home</p> }/>
+                <Route path="/work" element={ <p>work</p> }/>
+                <Route path="/about" element={ <p>about</p> }/>
+              </Routes>
+          </main>
+          <Sidebar />
+        </div>
+        <Footer />
+      </BrowserRouter>
 
-        </main>
-        <Sidebar />
-      </div>
-      <footer>
-        <p>footer</p>
-      </footer>
-      
     </div>
   );
 }
