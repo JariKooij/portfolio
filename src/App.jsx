66544b0bd75eb3d53fa6e300f72a423339f79e8a
JariKooij <jarikooij@gmail.com>
2023-01-03 03:10:34 +0100

    fixed svg errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function App() {
   useWindowBlurChangeTitle('Please, come back!');
-
   return (
     <div className="App">
-      <div id="wrapper">
-        <main>
-
-        </main>
-        <Sidebar />
-      </div>
-      <footer>
-        <p>footer</p>
-      </footer>
-      
+        <div id="wrapper">
+          <main>
+          </main>
+          <Sidebar />
+        </div>
+        <footer>
+          <p>footer</p>
+        </footer>
     </div>
+
   );
 }
